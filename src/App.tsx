2dda86dc6c7d15209b98987bd96714cad9a8f347
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-03 21:29:40 +0500

    Fontface Lato Installed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 
+// Font Family "Lato"
+import "@fontsource/lato";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/400-italic.css";
+
 function App() {
   return (
     <Routes>
