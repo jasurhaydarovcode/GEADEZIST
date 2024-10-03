b10bf91d270502789f75f2dabd379a7eb3be0514
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-03 17:14:58 +0500

    Cleared

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, Plugin, ViteDevServer } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const GEADEZIST = (): Plugin => {
+  return {
+    name: 'my-plugin',
+    configureServer(server: ViteDevServer) {
+      server.httpServer?.once('listening', () => {
+        console.log('Happy Hacking, 🫠 🫠 😴\n GEADEZIST');
+      });
+    },
+  };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), GEADEZIST()],
+  server: {
+    port: 9999,
+  },
+});
