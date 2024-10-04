5825fa5ea74d59d79c17117fc444ff00650df730
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-04 16:24:10 +0500

    React Toastify Added

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './css/index.css';
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer />
     </BrowserRouter>
   </StrictMode>,
 );
