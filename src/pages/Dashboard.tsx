1674bd972a81a11a193bd3ac272ba9a03a3cad48
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-04 21:44:42 +0500

    test container dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -139,7 +139,7 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen container">
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Navbar />
