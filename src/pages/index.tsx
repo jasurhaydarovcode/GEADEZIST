247c8fe0e7bc2119f3b24a4e4152c99c283ac83e
Ulugbek-Raxmatillayev <ulugbekraxmatillayev615@gmail.com>
2024-10-04 16:25:56 +0500

    update to add helpers folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
-import axios from "axios"
-import { AxiosResponse } from "axios";
 
 const AdminScreen = (): JSX.Element => {
-    function getApi () : void {
-        axios.get('url')
-            .then((res: AxiosResponse) => {console.log(res);
-            })
-    }
     return (
         <div></div>
     )
