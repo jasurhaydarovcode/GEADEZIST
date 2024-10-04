4ffb4c8b7b2531abca421d232ca9a699a4d48c3f
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-10-04 16:44:12 +0500

    NotFound page add

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,9 @@
 function NotFound() {
-  return <div>NotFound</div>;
+  return <div>
+        <main className="h-screen w-full flex flex-col justify-center items-center bg-[#a8a9ac]">
+          <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
+        </main>
+  </div>;
 }
 
 export default NotFound;
