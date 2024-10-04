8d23634f7346d7973e6eb43915a0adc496c951af
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-04 21:24:51 +0500

    updat

--- src/helpers/imports/images.ts
+++ src/helpers/imports/images.ts
@@ -1,4 +1,5 @@
 import Logo from '../../assets/images/logo.svg'
 import geodeziyaLogo from '../../assets/images/GEADEZIST.png'
+import registerRasm from '../../assets/images/register/register-sv.svg'
 
-export {Logo, geodeziyaLogo} 
+export {Logo, geodeziyaLogo, registerRasm} 
