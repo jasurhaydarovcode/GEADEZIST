1d83a379b3944fc91a4b1f30a05104ffd4e81bfb
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-05 12:06:06 +0500

    Some verstka client written

--- src/helpers/imports/images.ts
+++ src/helpers/imports/images.ts
@@ -1,5 +1,11 @@
+// GEADEZIST
 import Logo from '../../assets/images/logo.svg';
 import geodeziyaLogo from '../../assets/images/GEADEZIST.png';
+
+// Auth
 import registerRasm from '../../assets/images/register/register-sv.svg';
 
-export { Logo, geodeziyaLogo, registerRasm };
+export {
+    Logo, geodeziyaLogo,
+    registerRasm
+};
--- src/pages/Client/ClientDashboard.tsx
+++ src/pages/Client/ClientDashboard.tsx
@@ -1,5 +1,15 @@
-function ClientDashboard() {
-  return <div>ClientDashboard</div>;
-}
+// components/clientDashboard/ClientDashboard.tsx
+import Layout from '../../components/clientDashboard/laytout';
+
+const ClientDashboard: React.FC = () => {
+  return (
+    <Layout>
+      <div>
+        {/* This is where your result cards or other dashboard content will go */}
+        <h2 className='text-red-600'>Sizning Natijalaringiz</h2>
+      </div>
+    </Layout>
+  );
+};
 
 export default ClientDashboard;
