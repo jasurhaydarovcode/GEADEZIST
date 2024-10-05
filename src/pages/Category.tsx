b15c767d63f3e30a18127c17729ab8a65765b8ae
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-10-05 23:24:33 +0500

    Your commit message describing local changes

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,1 +1,36 @@
+import Sidebar from "@/components/Dashboard/Sidebar"
+import { Button } from "antd"
+import { Navbar } from "flowbite-react"
+import { useState } from "react";
+import { MdOutlineAddCircle } from 'react-icons/md';
+import Modal from 'react-modal';
 
+
+
+const Category = () => {
+    const [ isModalOpen, setIsModalOpen] = useState(false);
+  
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    return (
+        <>
+            <Navbar />
+            <div>
+                <Sidebar />
+                <div className={`mb-5`}>
+                    <Button
+                    onClick={openModal}
+                    children={<div className={`flex justify-center items-center`}>
+                        <MdOutlineAddCircle className={`text-4xl mr-3`} />
+                        <p className={`text-lg font-bold`}>Қўшиш</p>
+                    </div>}
+                    />
+                </div>
+                {isModalOpen && <Modal onRequestClose={closeModal} />}
+            </div>
+        </>
+    )
+}
+
+export default Category
