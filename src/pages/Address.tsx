55b552170e57d5edb6933208b5cd13021b5895ae
asilbekcodes <asilbeksuvonov43@gmail.com>
2024-10-06 15:09:38 +0500

    update

--- src/pages/Address.tsx
+++ src/pages/Address.tsx
@@ -40,6 +40,34 @@ function Address() {
           </Table>
           <Pagination className="mt-5" defaultCurrent={1} total={20} />
         </div>
+        <div className="flex justify-between items-center">
+          <p className="font-sans text-2xl text-gray-700">Tumanlar</p>
+          <Button color="default" variant="solid" className=" text-xl px-5 py-6 my-5">
+            <PlusCircleOutlined className="text-xl"/>Qo'shish 
+          </Button>
+        </div>
+        <div>
+        <Table hoverable>
+            <TableHead>
+              <TableHeadCell>T/P</TableHeadCell>
+              <TableHeadCell>Tuman nomi</TableHeadCell>
+              <TableHeadCell>Viloyat nomi</TableHeadCell>
+              <TableHeadCell>Harakat</TableHeadCell>
+            </TableHead>
+            <TableBody className="divide-y">
+              <TableRow className="bg-white text-gray-700 dark:border-gray-700 dark:bg-gray-800">
+                <TableCell>1</TableCell>
+                <TableCell>Chilonzor shahar</TableCell>
+                <TableCell>Toshkent</TableCell>
+                <TableCell className="flex gap-1 text-xl cursor-pointer">
+                  <MdEdit />
+                  <MdDelete />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <Pagination className="mt-5" defaultCurrent={1} total={30} />
+        </div>
       </div>
     </Layout>
   )
