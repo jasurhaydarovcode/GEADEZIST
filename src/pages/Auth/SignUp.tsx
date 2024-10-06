e7bc28f0d8b51bac32e2690d535524d64cc633d3
Javlon2 <choriyevjavlon5@gmail.com>
2024-10-06 21:19:08 +0500

    login_push-1

--- src/pages/Auth/SignUp.tsx
+++ src/pages/Auth/SignUp.tsx
@@ -80,6 +80,8 @@ function SignUp() {
                   required
                   className="w-full px-4 mt-2 py-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
                 />
+                <p className="text-xs ">Parol kamida 5 ta belgidan iborat</p>
+
               </div>
 
               {/* Parolni tasdiqlash */}
