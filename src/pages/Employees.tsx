1f548d7895eaa1558e5fbd7c94efd0e34d2f01a8
asilbekcodes <asilbeksuvonov43@gmail.com>
2024-10-06 13:32:53 +0500

    Pagination added

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -4,55 +4,57 @@ import { Button } from "antd"
 import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 import { Space, Switch } from 'antd';
+import { Pagination } from 'antd';
 
 function Employees() {
   return (
     <Layout>
       <div className="p-5">
-          <div className="flex justify-between">
-            <h1 className="text-3xl font-bold font-sans">Hodimlar</h1>
-            <p className="font-sans text-gray-700">
-              Boshqaruv paneli  /  <span className="text-blue-700 ">Hodimlar</span>
-            </p>
-          </div>
-          <div>
-            <Button color="default" variant="solid" className="text-xl px-5 py-6 my-5">
-              <PlusCircleOutlined className="text-xl"/>Qo'shish 
-            </Button>
-          </div>
-          <div className="overflow-x-auto">
-      <Table hoverable>
-        <TableHead>
-          <TableHeadCell>T/P</TableHeadCell>
-          <TableHeadCell>Ism</TableHeadCell>
-          <TableHeadCell>Familya</TableHeadCell>
-          <TableHeadCell>Electron pochta</TableHeadCell>
-          <TableHeadCell>Lavozimi</TableHeadCell>
-          <TableHeadCell>Action</TableHeadCell>
-        </TableHead>
-        <TableBody className="divide-y">
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Apple MacBook Pro 17"'}
-            </TableCell>
-            <TableCell>Sliver</TableCell>
-            <TableCell>Laptop</TableCell>
-            <TableCell>$2999</TableCell>
-            <TableCell>$2999</TableCell>
-            <TableCell>
-              <Space direction="vertical">
-                <Switch
-                  checkedChildren={<CheckOutlined />}
-                  unCheckedChildren={<CloseOutlined />}
-                  defaultChecked
-                />
-              </Space>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold font-sans">Hodimlar</h1>
+          <p className="font-sans text-gray-700">
+            Boshqaruv paneli  /  <span className="text-blue-700 ">Hodimlar</span>
+          </p>
         </div>
+        <div>
+          <Button color="default" variant="solid" className="text-xl px-5 py-6 my-5">
+            <PlusCircleOutlined className="text-xl"/>Qo'shish 
+          </Button>
+        </div>
+        <div>
+          <Table hoverable>
+            <TableHead>
+              <TableHeadCell>T/P</TableHeadCell>
+              <TableHeadCell>Ism</TableHeadCell>
+              <TableHeadCell>Familya</TableHeadCell>
+              <TableHeadCell>Electron pochta</TableHeadCell>
+              <TableHeadCell>Lavozimi</TableHeadCell>
+              <TableHeadCell>Action</TableHeadCell>
+            </TableHead>
+            <TableBody className="divide-y">
+              <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {'Apple MacBook Pro 17"'}
+                </TableCell>
+                <TableCell>Sliver</TableCell>
+                <TableCell>Laptop</TableCell>
+                <TableCell>$2999</TableCell>
+                <TableCell>$2999</TableCell>
+                <TableCell>
+                  <Space direction="vertical">
+                    <Switch
+                      checkedChildren={<CheckOutlined />}
+                      unCheckedChildren={<CloseOutlined />}
+                      defaultChecked
+                    />
+                  </Space>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <Pagination className="mt-5" defaultCurrent={1} total={10} />
+        </div>
+      </div>
     </Layout>
   )
 }
