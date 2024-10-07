821f151d2377d2be2a2b7ad7e0243bf7c242c91e
Ulugbek-Raxmatillayev <ulugbekraxmatillayev615@gmail.com>
2024-10-07 17:51:00 +0500

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import { getMe } from '@/helpers/api/baseUrl';
 import axios from 'axios';
+import { AxiosResponse } from 'node_modules/axios/index.d.cts';
 import Helmet from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
   const navigate = useNavigate()
   function getUser():void {
-    axios.get(getMe).then((res: ) => {
-
+    axios.get(getMe).then((res: AxiosResponse) => {
+        
     })
   }
   return (
