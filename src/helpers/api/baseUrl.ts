8929df516def2486103c44efb79c1c88914856f9
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-10-10 23:55:02 +0500

    TableCell img url add

--- src/helpers/api/baseUrl.ts
+++ src/helpers/api/baseUrl.ts
@@ -12,3 +12,19 @@ export const addCategory = `${baseUrl}category/add`
 export const getCategory = `${baseUrl}category/get/all`
 export const deleteCategory = `${baseUrl}category/delete`
 export const updateCategory = `${baseUrl}category/update`
+// video
+export const getVideo = `${baseUrl}video/get/all`
+export const addVideo = `${baseUrl}video/add`
+export const deleteVideo = `${baseUrl}video/delete`
+export const updateVideo = `${baseUrl}video/update`
+// video file
+export const getVideoFile = `${baseUrl}video/file/get/all`
+export const addVideoFile = `${baseUrl}video/file/add`
+export const deleteVideoFile = `${baseUrl}video/file/delete`
+export const updateVideoFile = `${baseUrl}video/file/update`
+
+//image
+export const getImage = `${baseUrl}api/videos/files`
+export const addImage = `${baseUrl}image/add`
+export const deleteImage = `${baseUrl}image/delete`
+export const updateImage = `${baseUrl}image/update`
--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,9 +1,10 @@
 import Layout from "@/components/Dashboard/Layout";
-import { baseUrl } from "@/helpers/api/baseUrl";
+import { baseUrl, getImage } from "@/helpers/api/baseUrl";
 import { config } from "@/helpers/functions/token";
 import axios from "axios";
 import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
 import { useQuery } from "react-query";
+import defaultImage from '../assets/images/default.png';
 
 function Category() {
   const data = useQuery(['getAddress'], async () => {
@@ -31,17 +32,22 @@ function Category() {
               {Array.isArray(data.data) && data.data.map((item, index) => (
                 <TableRow key={item.id} className="bg-white text-gray-900 dark:border-gray-700 dark:bg-gray-800">
                   <TableCell>{index + 1}</TableCell>
+                  <TableCell>
+                    <img 
+                      alt={item.name}
+                      src={item.fileId ? `${getImage}${item.fileId}` : defaultImage}
+                      className={'border-[1px] border-gray-300 w-[43px] h-[43px] rounded-full object-cover hover:cursor-pointer'}
+                    />
+                  </TableCell>
                   <TableCell>{item.name}</TableCell>
                   <TableCell>{item.description}</TableCell>
                   <TableCell>{item.questionCount}</TableCell>
                   <TableCell>{item.extraQuestionCoun}</TableCell>
-                  <TableCell>{item.durationTime}</TableCell>
-                  <TableCell>{item.retakeDat}</TableCell>
-                  <TableCell>{item.fileId}</TableCell>
-                  <TableCell>{item.updatedBy}</TableCell>   
-                  <TableCell>{item.main}</TableCell>
+                  <TableCell>{item.retakeDate}</TableCell>
                   <TableCell>{item.createdBy}</TableCell>
-                  <TableCell>{}</TableCell>
+                  <TableCell>{item.deleted && 'Uchirilgan'}</TableCell>
+                  <TableCell>{item.deletedBy}</TableCell>
+
                 </TableRow>
               ))}
             </TableBody>
