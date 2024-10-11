96f8532b8de857b99f3809ab99abc60413661aa1
asilbekcodes <asilbeksuvonov43@gmail.com>
2024-10-11 23:51:09 +0500

    update

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,83 +1,83 @@
-import Layout from '@/components/Dashboard/Layout';
-import { PlusCircleOutlined } from '@ant-design/icons';
-import { Button, Space, Switch, Pagination, Modal } from 'antd';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeadCell,
-  TableRow,
-} from 'flowbite-react';
-import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
-import { useEffect, useState } from 'react';
+import Layout from '@/components/Dashboard/Layout'; 
+import { PlusCircleOutlined } from '@ant-design/icons'; 
+import { Button, Space, Switch, Pagination, Modal } from 'antd'; 
+import { 
+  Table, 
+  TableBody, 
+  TableCell, 
+  TableHead, 
+  TableHeadCell, 
+  TableRow, 
+} from 'flowbite-react'; 
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons'; 
+import { useEffect, useState } from 'react'; 
 import { useMutation, useQuery } from 'react-query';
 import axios from 'axios';
-import { baseUrl } from '@/helpers/api/baseUrl';
-import { config } from '@/helpers/functions/token';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { baseUrl } from '@/helpers/api/baseUrl'; 
+import { config } from '@/helpers/functions/token'; 
+import { useNavigate } from 'react-router-dom'; 
+import { toast } from 'react-toastify'; 
 import TableLoading from '@/components/spinner/TableLoading';
 
-function Employees() {
+function Employees() { 
   const [open, setOpen] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
-  // Pagination holati
+  // Pagination holati 
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
   const [totalItems, setTotalItems] = useState(0);
 
-  function checkRoleClient() {
-    const role = localStorage.getItem('role');
-    if (role == 'ROLE_CLIENT') {
-      navigate('/client/dashboard');
-    }
-  }
+  function checkRoleClient() { 
+    const role = localStorage.getItem('role'); 
+    if (role == 'ROLE_CLIENT') { 
+      navigate('/client/dashboard'); 
+    } 
+  } 
+ 
+  const { data: admins, isLoading } = useQuery( 
+    ['getADmin', currentPage], 
+    async () => { 
+      const res = await axios.get( 
+        `${baseUrl}user/get/admin/list?page=${currentPage - 1}&size=${pageSize}`, 
+        config, 
+      ); 
+      const responseData = ( 
+        res.data as { 
+          body: { body: string; totalElements: number; totalPage: number }; 
+        } 
+      ).body; 
+      setTotalItems(responseData.totalElements); // Umumiy ma'lumotlar sonini saqlaymiz 
+      return responseData.body; 
+    }, 
+    { 
+      keepPreviousData: true, // Sahifa o'zgarganda eski ma'lumotlarni saqlab qoladi 
+    }, 
+  ); 
 
-  const { data: admins, isLoading } = useQuery(
-    ['getADmin', currentPage],
-    async () => {
-      const res = await axios.get(
-        `${baseUrl}user/get/admin/list?page=${currentPage - 1}&size=${pageSize}`,
-        config,
-      );
-      const responseData = (
-        res.data as {
-          body: { body: string; totalElements: number; totalPage: number };
-        }
-      ).body;
-      setTotalItems(responseData.totalElements); // Umumiy ma'lumotlar sonini saqlaymiz
-      return responseData.body;
-    },
-    {
-      keepPreviousData: true, // Sahifa o'zgarganda eski ma'lumotlarni saqlab qoladi
-    },
-  );
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page); // Hozirgi sahifani yangilash
-    setPageSize(pageSize);
+  const handlePageChange = (page: number) => { 
+    setCurrentPage(page); // Hozirgi sahifani yangilash 
+    setPageSize(pageSize); 
   };
 
-  const navigate = useNavigate();
-  useEffect(() => {
-    checkRoleClient();
-  }, [checkRoleClient]);
+  const navigate = useNavigate(); 
+  useEffect(() => { 
+    checkRoleClient(); 
+  }, [checkRoleClient]); 
 
-  const showModal = () => {
-    setOpen(true);
-  };
+  const showModal = () => { 
+    setOpen(true); 
+  };  
 
-  const handleOk = () => {
-    postAdmin.mutate();
-    setConfirmLoading(true);
-    setTimeout(() => {
-      setOpen(false);
-      setConfirmLoading(false);
-    }, 2000);
-  };
+  const handleOk = () => { 
+    postAdmin.mutate(); 
+    setConfirmLoading(true); 
+    setTimeout(() => { 
+      setOpen(false); 
+      setConfirmLoading(false); 
+    }, 2000); 
+  }; 
 
-  // const handleOk = () => {
+  // const handleOk = () => { 
   //   if (firstname && lastname && email && phoneNumber && password && confirmPassword && role) {
   //     if (password === confirmPassword) {
   //       postAdmin.mutate();
@@ -94,66 +94,66 @@ function Employees() {
   //   }
   // };
 
-  const handleCancel = () => {
-    setOpen(false);
+  const handleCancel = () => { 
+    setOpen(false); 
   };
 
-  // Hodim holatini yangilash uchun mutatsiya yaratish
-  const updateEmployeeStatus = useMutation(
-    async ({ id, enabled }: { id: string; enabled: boolean }) => {
-      return axios.put(`${baseUrl}user/active/${id}`, { enabled }, config);
-    },
-    {
-      // Mutatsiya muvaffaqiyatli bo'lganda, hodimlar ro'yxatini toastga chiqarish
-      onSuccess: (data, variables) => {
-        const { enabled } = variables;
-        console.log('Yangilandi:', data);
-        // queryClient.invalidateQueries('getADmin'); // Adminlar ma'lumotini qayta yuklash
-        if (enabled === true) {
-          toast.success('Hodim muvaffaqiyatli ishga tushirildi');
-        } else {
-          toast.success("Hodim muvaffaqiyatli o'chirildi");
-        }
-      },
-      onError: (error) => {
-        console.error('Xatolik:', error);
-      },
-    },
-  );
+  // Hodim holatini yangilash uchun mutatsiya yaratish 
+  const updateEmployeeStatus = useMutation( 
+    async ({ id, enabled }: { id: string; enabled: boolean }) => { 
+      return axios.put(`${baseUrl}user/active/${id}`, { enabled }, config); 
+    }, 
+    { 
+      // Mutatsiya muvaffaqiyatli bo'lganda, hodimlar ro'yxatini toastga chiqarish 
+      onSuccess: (data, variables) => { 
+        const { enabled } = variables; 
+        console.log('Yangilandi:', data); 
+        // queryClient.invalidateQueries('getADmin'); // Adminlar ma'lumotini qayta yuklash 
+        if (enabled === true) { 
+          toast.success('Hodim muvaffaqiyatli ishga tushirildi'); 
+        } else { 
+          toast.success("Hodim muvaffaqiyatli o'chirildi"); 
+        } 
+      }, 
+      onError: (error) => { 
+        console.error('Xatolik:', error); 
+      }, 
+    }, 
+  ); 
 
-  // Switch o'zgarganda ishlaydigan funksiya
-  const handleSwitchChange = (checked: boolean, id: string) => {
-    updateEmployeeStatus.mutate({ id, enabled: checked });
-  };
+  // Switch o'zgarganda ishlaydigan funksiya 
+  const handleSwitchChange = (checked: boolean, id: string) => { 
+    updateEmployeeStatus.mutate({ id, enabled: checked }); 
+  }; 
 
-  // yangi hodim qo'shish funksiyasi
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [role, setRole] = useState('');
+  // yangi hodim qo'shish funksiyasi 
+  const [firstname, setFirstname] = useState(''); 
+  const [lastname, setLastname] = useState(''); 
+  const [email, setEmail] = useState(''); 
+  const [phoneNumber, setPhoneNumber] = useState(''); 
+  const [password, setPassword] = useState(''); 
+  const [confirmPassword, setConfirmPassword] = useState(''); 
+  const [role, setRole] = useState(''); 
 
-  const postAdmin = useMutation(async () => {
-    return axios.post(
-      `${baseUrl}auth/save/admin`,
-      {
-        firstname,
-        lastname,
-        email,
-        phoneNumber,
-        password,
-        confirmPassword,
-        role,
-      },
-      config,
-    );
-  });
+  const postAdmin = useMutation(async () => { 
+    return axios.post( 
+      `${baseUrl}auth/save/admin`, 
+      { 
+        firstname, 
+        lastname, 
+        email, 
+        phoneNumber, 
+        password, 
+        confirmPassword, 
+        role, 
+      }, 
+      config, 
+    ); 
+  }); 
 
-  return (
-    <Layout>
-      {isLoading ?(
+  return ( 
+    <Layout> 
+      {isLoading ?( 
         <div className='flex justify-center items-center h-[80vh]'>{<TableLoading />}</div>
       ) : (
       <div className="p-5">
@@ -164,14 +164,14 @@ function Employees() {
           </p>
         </div>
         <div>
-          <Button
-            onClick={showModal}
-            color="default"
-            variant="solid"
-            className="text-xl px-5 py-6 my-5"
-          >
-            <PlusCircleOutlined className="text-xl" /> Qo'shish
-          </Button>
+          <Button 
+            onClick={showModal} 
+            color="default" 
+            variant="solid" 
+            className="text-xl px-5 py-6 my-5" 
+          > 
+            <PlusCircleOutlined className="text-xl" /> Qo'shish 
+          </Button> 
           <Modal
             title="Hodim qo'shish"
             open={open}
@@ -184,75 +184,75 @@ function Employees() {
             <div className="mb-4">
               {/* <label className="block mb-2">Admin toifasini tanlang</label> */}
               <select
-                className="border w-full p-2 rounded"
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-              >
-                <option value="">Admin toifasini tanlang</option>
+                className="border w-full p-2 rounded" 
+                value={role} 
+                onChange={(e) => setRole(e.target.value)} 
+              > 
+                <option value="">Admin toifasini tanlang</option> 
                 <option value="ROLE_TESTER">Tester admin</option>
                 <option value="ROLE_ADMIN">Tekshiruvchi admin</option>
-              </select>
+              </select> 
             </div>
             <div className="mb-4">
               <label className="block mb-2">Ism</label>
               <input
-                type="text"
-                placeholder="Ismni kiriting"
-                className="border w-full p-2 rounded"
-                value={firstname}
-                onChange={(e) => setFirstname(e.target.value)}
-              />
+                type="text" 
+                placeholder="Ismni kiriting" 
+                className="border w-full p-2 rounded" 
+                value={firstname} 
+                onChange={(e) => setFirstname(e.target.value)} 
+              /> 
             </div>
             <div className="mb-4">
               <label className="block mb-2">Ism</label>
               <input
-                type="text"
-                placeholder="Ismni kiriting"
-                className="border w-full p-2 rounded"
-                value={lastname}
-                onChange={(e) => setLastname(e.target.value)}
-              />
+                type="text" 
+                placeholder="Ismni kiriting" 
+                className="border w-full p-2 rounded" 
+                value={lastname} 
+                onChange={(e) => setLastname(e.target.value)} 
+              /> 
             </div>
             <div className="mb-4">
               <label className="block mb-2">Telfon raqam</label>
-              <input
-                type="text"
-                placeholder="Telfon raqamni kiriting"
-                className="border w-full p-2 rounded"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-              />
+              <input 
+                type="text" 
+                placeholder="Telfon raqamni kiriting" 
+                className="border w-full p-2 rounded" 
+                value={phoneNumber} 
+                onChange={(e) => setPhoneNumber(e.target.value)} 
+              /> 
             </div>
-            <div className="mb-4">
-              <label className="block mb-2">Email kiriting</label>
-              <input
-                type="email"
-                placeholder="Email kiriting"
-                className="border w-full p-2 rounded"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+            <div className="mb-4"> 
+              <label className="block mb-2">Email kiriting</label> 
+              <input 
+                type="email" 
+                placeholder="Email kiriting" 
+                className="border w-full p-2 rounded" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+              /> 
+            </div> 
+            <div className="mb-4"> 
+              <label className="block mb-2">Parolni kiriting</label> 
+              <input 
+                type="text"  
+                placeholder="Parolni kiriting" 
+                className="border w-full p-2 rounded" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
               />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-2">Parolni kiriting</label>
-              <input
-                type="text"
-                placeholder="Parolni kiriting"
-                className="border w-full p-2 rounded"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-2">Parolni takrorlang</label>
-              <input
-                type="text"
-                placeholder="Takroriy parolni kiriting"
-                className="border w-full p-2 rounded"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </div>
+            </div> 
+            <div className="mb-4"> 
+              <label className="block mb-2">Parolni takrorlang</label> 
+              <input 
+                type="text" 
+                placeholder="Takroriy parolni kiriting" 
+                className="border w-full p-2 rounded" 
+                value={confirmPassword} 
+                onChange={(e) => setConfirmPassword(e.target.value)} 
+              /> 
+            </div> 
           </Modal>
         </div>
         <div>
@@ -266,49 +266,49 @@ function Employees() {
               <TableHeadCell>Action</TableHeadCell>
             </TableHead>
             <TableBody className="divide-y">
-              {Array.isArray(admins) &&
-                admins.map((item, index) => (
-                  <TableRow
-                    key={item.id}
-                    className="bg-white text-gray-900 dark:border-gray-700 dark:bg-gray-800"
-                  >
-                    <TableCell>{index + 1}</TableCell>
-                    <TableCell>{item.firstName}</TableCell>
-                    <TableCell>{item.lastName}</TableCell>
-                    <TableCell>{item.email}</TableCell>
-                    <TableCell>
-                      {item.role === 'ROLE_ADMIN'
-                        ? 'Tekshiruvchi admin'
-                        : 'Tester admin'}
-                    </TableCell>
-                    <TableCell>
-                      <Space direction="vertical">
-                        <Switch
-                          checkedChildren={<CheckOutlined />}
-                          unCheckedChildren={<CloseOutlined />}
-                          defaultChecked={item.enabled}
-                          onChange={(checked) =>
-                            handleSwitchChange(checked, item.id)
-                          }
-                        />
-                      </Space>
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-          <Pagination
-            className="mt-5"
-            current={currentPage}
-            total={totalItems}
-            pageSize={pageSize}
-            onChange={handlePageChange}
-          />
-        </div>
-      </div>
-      )}
-    </Layout>
+              {Array.isArray(admins) && 
+                admins.map((item, index) => ( 
+                  <TableRow 
+                    key={item.id} 
+                    className="bg-white text-gray-900 dark:border-gray-700 dark:bg-gray-800" 
+                  > 
+                    <TableCell>{index + 1}</TableCell> 
+                    <TableCell>{item.firstName}</TableCell> 
+                    <TableCell>{item.lastName}</TableCell> 
+                    <TableCell>{item.email}</TableCell> 
+                    <TableCell> 
+                      {item.role === 'ROLE_ADMIN' 
+                        ? 'Tekshiruvchi admin' 
+                        : 'Tester admin'} 
+                    </TableCell> 
+                    <TableCell> 
+                      <Space direction="vertical"> 
+                        <Switch 
+                          checkedChildren={<CheckOutlined />} 
+                          unCheckedChildren={<CloseOutlined />} 
+                          defaultChecked={item.enabled} 
+                          onChange={(checked) => 
+                            handleSwitchChange(checked, item.id) 
+                          } 
+                        /> 
+                      </Space> 
+                    </TableCell> 
+                  </TableRow> 
+                ))} 
+            </TableBody> 
+          </Table> 
+          <Pagination 
+            className="mt-5" 
+            current={currentPage} 
+            total={totalItems} 
+            pageSize={pageSize} 
+            onChange={handlePageChange} 
+          /> 
+        </div> 
+      </div> 
+      )} 
+    </Layout> 
   );
-}
-
+} 
+ 
 export default Employees;
