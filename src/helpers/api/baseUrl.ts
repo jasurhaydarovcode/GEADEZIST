b092e5fe0a25c4f094780893159f2119561ae4e0
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-11 15:32:35 +0500

    BaseUrl Updated

--- src/helpers/api/baseUrl.ts
+++ src/helpers/api/baseUrl.ts
@@ -1,22 +1,27 @@
+// ======= Swagger UI =======
 export const baseUrl = 'http://164.92.165.18:8090/'
+
 export const loginUrl = `${baseUrl}auth/login`
+
+// ==================== User Controller ====================
 export const getMeUser = `${baseUrl}user/get/me`
-export const getStaticAll = `${baseUrl}statistic/counts/`
 export const getClientAll = `${baseUrl}user?page=0&size=10`
 export const getMe = `${baseUrl}user/get/me`
 export const getProfile = `${baseUrl}user/profile`
-export const getClientCategory = `${baseUrl}category/`
 
 // category
+export const getClientCategory = `${baseUrl}category/`
 export const addCategory = `${baseUrl}category/add`
 export const getCategory = `${baseUrl}category/get/all`
 export const deleteCategory = `${baseUrl}category/delete`
 export const updateCategory = `${baseUrl}category/update`
+
 // video
 export const getVideo = `${baseUrl}video/get/all`
 export const addVideo = `${baseUrl}video/add`
 export const deleteVideo = `${baseUrl}video/delete`
 export const updateVideo = `${baseUrl}video/update`
+
 // video file
 export const getVideoFile = `${baseUrl}video/file/get/all`
 export const addVideoFile = `${baseUrl}video/file/add`
@@ -28,3 +33,19 @@ export const getImage = `${baseUrl}api/videos/files`
 export const addImage = `${baseUrl}image/add`
 export const deleteImage = `${baseUrl}image/delete`
 export const updateImage = `${baseUrl}image/update`
+
+
+
+
+// ==================== statistics-controller ====================
+// User dashboard reultlari
+export const getUserDashboard = `${baseUrl}statistic/user-dashboard`
+
+// SUPTER ADMIN dashboard userlarni resutlari filter bo'yicha
+export const superAdminDashboardUsersResultFilter = `${baseUrl}statistic/filter`
+
+// SUPER ADMIN dashboard haftalik statistika
+export const superAdminDashWeekStatistic = `${baseUrl}statistic/filter`
+
+// SUPER ADMIN dashboard barcha statstika sonlarda
+export const getStaticAll = `${baseUrl}statistic/counts`
