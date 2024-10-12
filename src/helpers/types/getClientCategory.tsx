f58f19f5392ceb1312c212ba9ef2d7cc9f31228a
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-12 16:23:10 +0500

    Merge Compileceted

--- src/helpers/types/getClientCategory.tsx
+++ src/helpers/types/getClientCategory.tsx
@@ -1,5 +1,4 @@
 export interface ClientCategory {
-<<<<<<< HEAD
     "name": string,
     "id": number,
     "duration": number,
@@ -7,12 +6,3 @@ export interface ClientCategory {
     "retakeDate": string,
     "fileId": string;
 }
-=======
-  name: string;
-  id: number;
-  duration: number;
-  questionCount: number;
-  retakeDate: string;
-  fileId: null;
-}
->>>>>>> 473b908fdf95dc5f455bc2b1070e32647a378885
