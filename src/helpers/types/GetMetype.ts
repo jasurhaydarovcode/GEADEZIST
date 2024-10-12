143e82fe7f6329c699a00c14fbbd9618b97c6ff9
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-12 16:18:06 +0500

    client profile changes

--- src/helpers/types/GetMetype.ts
+++ src/helpers/types/GetMetype.ts
@@ -8,4 +8,13 @@ export interface GetMetype {
   fullName?: string;
   email?: string;
   fileId?: null;
+
+  firstName?: string;
+  lastName? : string;
+  region? : string;
+  district? : string;
+  address? : string;
+  phoneNumber? : string;
+  dateOfBirth? : string;
+  profileImage? : string;
 }
