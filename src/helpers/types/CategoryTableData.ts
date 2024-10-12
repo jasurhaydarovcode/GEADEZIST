d807ad5c144a20e9b667420b7fea67f18ebc6238
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-10-12 21:08:22 +0500

    CategoryTableData ishlatsa bo'ladi

--- src/helpers/types/CategoryTableData.ts
+++ src/helpers/types/CategoryTableData.ts
@@ -1,14 +1,14 @@
-// export interface CategoryTableData {
-//   id: string;
-//   image: string;
-//   categoryType: 'asosiy' | 'asosiy-bolmagan';
-//   name: string;
-//   description: string;
-//   totalQuestions: number;
-//   additionalQuestions: number;
-//   duration: number;
-//   reAdmissionDate: string;
-//   createdBy: string;
-//   situation: string;
-//   deletedBy: string;
-// }
+export interface CategoryTableData {
+  id: string;
+  image: string;
+  categoryType: 'asosiy' | 'asosiy-bolmagan';
+  name: string;
+  description: string;
+  totalQuestions: number;
+  additionalQuestions: number;
+  duration: number;
+  reAdmissionDate: string;
+  createdBy: string;
+  situation: string;
+  deletedBy: string;
+}
