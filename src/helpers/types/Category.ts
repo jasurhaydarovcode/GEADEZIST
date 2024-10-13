e20481ff5ad5089357f89f6feded3b3e31d20e97
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-10-13 12:26:32 +0500

    export interface Category

--- src/helpers/types/Category.ts
+++ src/helpers/types/Category.ts
@@ -1,1 +1,14 @@
-
+// types/category.ts
+export interface Category {
+  id: string;
+  name: string;
+  description: string;
+  fileId?: string;
+  questionCount: number;
+  extraQuestionCount: number;
+  durationTime: number;
+  retakeDate: string;
+  createdBy: string;
+  deleted: boolean;
+  deletedBy?: string;
+}
