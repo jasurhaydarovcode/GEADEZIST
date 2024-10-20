7fbe31ba5a84f753ad88e22fff4540960d3382e1
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-10-20 15:58:18 +0500

    media full bitdi / add categoriya bitdi / edit ham full tugadi

--- src/pages/Admin/Category.tsx
+++ src/pages/Admin/Category.tsx
@@ -130,7 +130,7 @@ function Category() {
 
             <CategoryAddModal onAddCategory={handleAddCategory} />
 
-            <div className="container mx-auto w-[1130px] overflow-x-auto">
+            <div className="container mx-auto w-full max-w-[1130px] md:overflow-x-auto sm:overflow-x-hidden">
               <Table hoverable className="border-collapse table-auto">
                 <TableHead className="hidden sm:table-header-group">
                   {/* Kichik ekranlar uchun sarlavhani yashirish */}
