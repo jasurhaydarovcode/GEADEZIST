5d77c9ca91ec4f2cd52ee8f281140143d3152608
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-22 23:51:13 +0500

    Client dashboard agar data mavjuda bolmasa text yozib qoyildi

--- src/pages/Client/ClientDashboard.tsx
+++ src/pages/Client/ClientDashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useCallback, useState } from 'react';
 import Layout from '@/components/Dashboard/Layout';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { getResultsClient, getMeUser } from '@/helpers/api/baseUrl';
 import { useQuery } from 'react-query';
@@ -85,55 +85,72 @@ const ClientDashboard: React.FC = () => {
                 {getUser?.fullName || 'Foydalanuvchi nomi yuklanmoqda...'}
               </h4>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-              {cards && cards.length > 0 && cards.map((card: ClientRezType, index) => (
-                <div key={index} className="card bg-white glass w-96 mx-auto">
-                  <figure>
-                    <img
-                      src={card.categoryImage || defaultImageDash}
-                      alt="Category Image!"
-                      className="w-full h-64 object-cover"
-                    />
-                  </figure>
-                  <div className="card-body flex flex-col leading-6 justify-between">
-                    <div>
-                      <h2 className="card-title text-red-500 pb-5 font-bold text-center flex justify-center">
-                        {card.categoryName || <del className='text-black'>No Title Data</del>}
-                      </h2>
+            <div className="p-4">
+              {cards && cards.length > 0 ? (
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                  {cards.map((card: ClientRezType, index) => (
+                    <div key={index} className="card bg-white glass w-96 mx-auto">
+                      <figure>
+                        <img
+                          src={card.categoryImage || defaultImageDash}
+                          alt="Category Image!"
+                          className="w-full h-64 object-cover"
+                        />
+                      </figure>
+                      <div className="card-body flex flex-col leading-6 justify-between">
+                        <div>
+                          <h2 className="card-title text-red-500 pb-5 font-bold text-center flex justify-center">
+                            {card.categoryName || <del className='text-black'>No Title Data</del>}
+                          </h2>
 
-                      <p className="text-gray-600">
-                        <strong>Tog'ri Javoblar:</strong>
-                        <span className="float-right">{card.countAnswers}</span>
-                      </p>
-                      <p className="text-gray-600">
-                        <strong>Vaqt Davomiyligi:</strong>
-                        <span className="float-right">{card.durationTime} (daq.)</span>
-                      </p>
-                      <p className="text-gray-600">
-                        <strong>Tuplangan Ball:</strong>
-                        <span className="float-right">{card.testScore}</span>
-                      </p>
-                      <p className="text-gray-600">
-                        <strong>Test Topshirilgan vaqt:</strong>
-                        <span className="float-right">{card.createdAt}</span>
-                      </p>
+                          <p className="text-gray-600">
+                            <strong>Tog'ri Javoblar:</strong>
+                            <span className="float-right">{card.countAnswers}</span>
+                          </p>
+                          <p className="text-gray-600">
+                            <strong>Vaqt Davomiyligi:</strong>
+                            <span className="float-right">{card.durationTime} (daq.)</span>
+                          </p>
+                          <p className="text-gray-600">
+                            <strong>Tuplangan Ball:</strong>
+                            <span className="float-right">{card.testScore}</span>
+                          </p>
+                          <p className="text-gray-600">
+                            <strong>Test Topshirilgan vaqt:</strong>
+                            <span className="float-right">{card.createdAt}</span>
+                          </p>
 
-                      <h2 className="card-title text-red-500 pt-5 text-lg font-bold text-center flex justify-center">
-                        Qo'shimcha yo'nalishlardan ishlanganlar
-                      </h2>
+                          <h2 className="card-title text-red-500 pt-5 text-lg font-bold text-center flex justify-center">
+                            Qo'shimcha yo'nalishlardan ishlanganlar
+                          </h2>
+                        </div>
+
+                        <div className="card-actions mt-4">
+                          <button
+                            className={`btn text-lg w-full ${card.status === 'APPROVED' ? 'btn-success' : card.status === 'WAITING' ? 'btn-warning' : 'btn-primary'}`}
+                          >
+                            {card.status === 'APPROVED' ? 'Tasdiqlandi' : card.status === 'WAITING' ? 'Kutilmoqda' : card.status}
+                          </button>
+                        </div>
+                      </div>
                     </div>
-
-                    <div className="card-actions mt-4">
-                      <button
-                        className={`btn text-lg w-full ${card.status === 'APPROVED' ? 'btn-success' : card.status === 'WAITING' ? 'btn-warning' : 'btn-primary'}`}
-                      >
-                        {card.status === 'APPROVED' ? 'Tasdiqlandi' : card.status === 'WAITING' ? 'Kutilmoqda' : card.status}
-                      </button>
-                    </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
+              ) : (
+                <div className="flex justify-center items-center w-full h-full p-10 bg-gray-100 rounded-md shadow-md">
+                  <p className="text-center text-red-600 font-semibold text-2xl">
+                    Siz xozircha test ishlamagansiz 😊,
+                    <span className='bg-red-500 text-white rounded-lg mx-2 py-1 px-3'>
+                      <Link to={"/client/test/start"}>
+                        test
+                      </Link>
+                    </span>
+                    ishlash uchun test bo'limiga o'ting
+                  </p>
+                </div>
+              )}
             </div>
+
           </div>
         </div>
       </Layout>
