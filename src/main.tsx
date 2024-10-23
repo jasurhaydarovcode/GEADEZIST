d7e528cb36073d65e5090bc20efdcf1cc5760258
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-23 20:02:27 +0500

    Updated React-Mower

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        {/* <CursorProtect> */}
+        {/* <CursorProtect message='❌ Websayt Ximoyalangan'> */}
           {/* <DevToolsBlocker> */}
             <App />
           {/* </DevToolsBlocker> */}
