3dbd2b7746564c28d3cc1c99c8eeda4893495368
Ulugbek-Raxmatillayev <ulugbekraxmatillayev615@gmail.com>
2024-10-24 01:25:28 +0500

    Commit 187: 10 qator kod qo'shildi

--- fayl.c
+++ fayl.c
@@ -4686,3 +4686,11 @@ printf("Result: %d
 ", c186);
 // End of block 186
 
+// Code block 187
+int a187 = 187;
+int b187 = 374;
+int c187 = a187 + b187;
+printf("Result: %d
+", c187);
+// End of block 187
+
