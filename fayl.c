904253e90113dd5455b5b6ab8afe32acdd90317a
Ulugbek-Raxmatillayev <ulugbekraxmatillayev615@gmail.com>
2024-10-24 01:25:32 +0500

    Commit 199: 10 qator kod qo'shildi

--- fayl.c
+++ fayl.c
@@ -4782,3 +4782,11 @@ printf("Result: %d
 ", c198);
 // End of block 198
 
+// Code block 199
+int a199 = 199;
+int b199 = 398;
+int c199 = a199 + b199;
+printf("Result: %d
+", c199);
+// End of block 199
+
